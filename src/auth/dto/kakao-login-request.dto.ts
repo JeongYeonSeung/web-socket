bc18602e4daf22fcf229fc1ddec6ec92b4e836db
JeongYeonSeung <asas4654@daum.net>
2024-11-08 15:09:45 +0900

    fix: validation 추가

--- src/auth/dto/kakao-login-request.dto.ts
+++ src/auth/dto/kakao-login-request.dto.ts
@@ -1,6 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class KakaoLoginRequestDto {
   @ApiProperty({ description: '인가 코드' })
+  @IsString()
+  @IsNotEmpty()
   code: string;
 }
